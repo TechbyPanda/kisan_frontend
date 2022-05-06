8d75cf6e643ee91c06854da8b17074e25f3efcc3
26palash <95613328+26palash@users.noreply.github.com>
2022-05-06 15:19:01 +0530

    service booking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
 
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 import { RegistrationPageComponent } from './registration-page/registration-page.component';
 import { SigninComponent } from './signin/signin.component';
 import { Navbar2Component } from './navbar2/navbar2.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
-
+import { MatInputModule } from '@angular/material/input'
 import { ContractFarmingComponent } from './contract-farming/contract-farming.component';
 
 import { EquipmentsComponent } from './equipments/equipments.component';
+import { Navbar3Component } from './navbar3/navbar3.component';
+import { EquipementsDetailsComponent } from './equipements-details/equipements-details.component';
 
 @NgModule({
   declarations: [
@@ -34,6 +40,8 @@ import { EquipmentsComponent } from './equipments/equipments.component';
     ContractFarmingComponent,
 
     EquipmentsComponent,
+      Navbar3Component,
+      EquipementsDetailsComponent,
 
   ],
   imports: [
@@ -43,10 +51,15 @@ import { EquipmentsComponent } from './equipments/equipments.component';
     MatSliderModule,
     MatToolbarModule,
     MatIconModule,
-    ToastrModule,
     MatCardModule,
     HttpClientModule,
     FormsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatNativeDateModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/navbar2/navbar2.component.ts
+++ src/app/navbar2/navbar2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar2',
@@ -10,6 +10,16 @@ export class Navbar2Component implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public fixed:boolean = false;
+
+  @HostListener('window:scroll',['$event']) onScroll(){
+    if(window.scrollY > 100){
+      this.fixed = true;
+    }else{
+      this.fixed=false;
+    }
   }
 
 }
