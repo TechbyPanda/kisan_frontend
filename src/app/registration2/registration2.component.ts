ec3a3a99cbec14a0f989b7b328fa8b2034debaef
devikakushwah <95033933+devikakushwah@users.noreply.github.com>
2022-05-11 12:01:19 +0530

    devika:

--- src/app/registration2/registration2.component.ts
+++ src/app/registration2/registration2.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from 'src/app/model/user';
+import { Router} from '@angular/router'
+import { HttpErrorResponse } from '@angular/common/http';
+import {UserService} from '../service/user.service';
 @Component({
   selector: 'app-registration2',
   templateUrl: './registration2.component.html',
@@ -7,11 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Registration2Component implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService,private router : Router) { }
 
   ngOnInit(): void {
   }
-
+  user:User= new User("","","","","","");
   hide=true;
+  public register(){
+    alert("data");
+    this.userService.User_Signup(this.user).subscribe(data=>{
+      alert(data.msg);
+      alert(data);
+      if(!data)
+      alert("data not found");
+      this.router.navigate(['']);
+    },err=>{
+      console.log(err);
+      if(err instanceof HttpErrorResponse){
+        if(err.status == 401){
+          alert(err);
+        }
+        else if(err.status == 500){
 
+        alert(err);
+      }
+    }
+  }
+    )
+  }
 }
