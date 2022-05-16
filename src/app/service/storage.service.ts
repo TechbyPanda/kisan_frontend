adbceb97aac5e867edc435117bcccdef919f748f
26palash <95613328+26palash@users.noreply.github.com>
2022-05-16 15:59:00 +0530

    storage booked

--- src/app/service/storage.service.ts
+++ src/app/service/storage.service.ts
@@ -8,10 +8,16 @@ import { HttpClient } from '@angular/common/http'
 export class StorageService {
 
   takeapi = "http://localhost:3000/storage/view-storage";
+  customerStorage = "http://localhost:3000/booked-storage/add/";
 
   constructor(private http:HttpClient) { }
 
   getStorage():Observable<any>{
     return this.http.get(this.takeapi)
+  }
+
+  bookStorage(sid:any,total:any,items:any,duration:any):Observable<any>{
+    var uid = sessionStorage.getItem('id');
+    return this.http.post(this.customerStorage+''+uid+'/'+sid,{items,total,duration})
   }
 }
