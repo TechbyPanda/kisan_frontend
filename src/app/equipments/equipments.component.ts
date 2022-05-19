c64935fd08d75a28e057af0276f558d0a9ac0b34
devikakushwah <95033933+devikakushwah@users.noreply.github.com>
2022-05-19 17:47:03 +0530

    devika

--- src/app/equipments/equipments.component.ts
+++ src/app/equipments/equipments.component.ts
@@ -5,7 +5,9 @@ import { UserService } from '../service/user.service';
 import { Router } from '@angular/router';
 import { Service } from '../model/service';
 import { ServicesService } from '../service/services.service';
+import { HttpErrorResponse } from '@angular/common/http';
 import { ServiceDialogComponent } from '../service-dialog/service-dialog.component';
+import { ToastrService } from 'ngx-toastr';
 declare let Razorpay:any
 @Component({
   selector: 'app-equipments',
@@ -18,33 +20,59 @@ export class EquipmentsComponent implements OnInit {
   paginate:any;
   totalLength?:number;
   page:number = 1;
+   mobile:any;
 
-
-  constructor(private dataService:ServicesService,public dialog: MatDialog,private adminService : AdminService,private userService: UserService,private router:Router) { }
+  constructor(private dataService:ServicesService,private notifyService:ToastrService,public dialog: MatDialog,private adminService : AdminService,private userService: UserService,private router:Router) { }
 
 
   service: Service = new Service("", "", "", "", false, false,"","");
-
-  num:any;
-  
-  orderList:any;
-  days:any;
-  date:any;
+  bookingDate:any;
+  username:any;
+  orderList:any=[];
   total:any;
+  address:any;
  id:any= sessionStorage.getItem("id");
   tid:any;
   price:any;
   name:any;
   ngOnInit(): void {
+    this.username = sessionStorage.getItem("name");
     this.adminService.service_Api().subscribe(data=>{
       this.tools = data
       this.totalLength =data.length;
     })
   }
+  
+
+  items:any=[]
+  single_items:any='';
+
+  duration:any;
+
+
+
+
+  isLoggedIn(){
+    return this.userService.checkToken();
+  }
+
+  public checkWeight(event:any){
+    
+    if(event.target.value){
+      this.total = this.price  * event.target.value*1;
+      this.service.duration = event.target.value;
+    }
+  }
+  setdata(items:any){
+    this.single_items=items;
+    console.log(items._id);
+  }
+  
   setData(id:any,price:any,name:any){
       this.tid = id;
       this.price = price;
       this.name = name;
+      this.total = price;
   }
   service_item(id:any){
         this.router.navigate(['equipment-details',id]);
@@ -82,15 +110,7 @@ onPay(amount:any){
   var rzp1 = new Razorpay(options);
 
     if(rzp1.open()){
-      this.orderList = [{bookingDate:this.date,tool_id:this.tid}];
-      this.service.userId = this.id;
-      this.service.payment = true;
-      this.service.total = this.price;
-     this.service.orderList = this.orderList;
-     this.dataService.serviceOrder(this.service).subscribe(data =>{
-       alert(data);
-       
-     })
+      alert("successfully payment")
     }
     else{
         alert("unpayment");
@@ -104,21 +124,35 @@ onPay(amount:any){
   }
   
   
-  getData(event:any){
-    
-    this.price = (this.price*1) * (event.target.value)*1;
-  
-    
-  }
-  isLoggedIn(){
-    return this.userService.checkToken();
-  }
+  selected:any;
   save(){
+    alert("data");
     if(this.isLoggedIn()){
-      
-      this.onPay(this.price);
-     
-    }
+      this.onPay(this.total);
+      this.orderList = [{bookingDate:this.bookingDate,tool_id:this.tid}];
+
+      this.service.userId = this.id;
+      this.service.payment = true;
+      this.service.total = this.total;
+     this.service.orderList = this.orderList;
+     this.dataService.serviceOrder(this.service).subscribe(data =>{
+       alert(data);
+       console.log(data);
+       this.notifyService.success("Order Booked Successfully..!!")
+
+      },err=>{
+       console.log(err);
+       if(err instanceof HttpErrorResponse){
+         if(err.status == 400){
+           this.notifyService.error("user already exists...");
+         }
+         else if(err.status == 500){
+           this.notifyService.warning("Something is wrong..!")
+         // alert(err);
+       }
+     }
+     })
+  }
     else{
         this.router.navigate(['sign-in']);
     }
--- src/app/model/service.ts
+++ src/app/model/service.ts
@@ -2,3 +2,7 @@ export class Service {
     constructor(public userId: string, public total:String,public address:string,public mobile:String,
     public shipping: boolean,public payment: boolean,public duration:string,public orderList:any){}
 }
+
+
+
+
