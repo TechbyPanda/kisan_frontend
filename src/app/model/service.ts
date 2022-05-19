bbc92488bd4259867d46c184578b868c481c7b01
devikakushwah <95033933+devikakushwah@users.noreply.github.com>
2022-05-19 22:42:05 +0530

    comment box

--- src/app/model/service.ts
+++ src/app/model/service.ts
@@ -1,5 +1,5 @@
 export class Service {
-    constructor(public userId: string, public total:String,public address:string,public mobile:String,
+    constructor(public userId: string, public total:any,public address:string,public mobile:String,
     public shipping: boolean,public payment: boolean,public duration:string,public orderList:any){}
 }
 
