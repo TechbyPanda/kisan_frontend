c3272d3402fa211f92636630d0101dd143931487
devikakushwah <95033933+devikakushwah@users.noreply.github.com>
2022-05-20 11:19:44 +0530

    table

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog,  MatDialogConfig} from '@angular/material/dialog';
 import { ServicesService } from '../service/services.service';
+import { HistoryDetailsComponent} from '../history-details/history-details.component';
 // export interface History {
 //   charges: string;
 //   address: string;
@@ -17,20 +19,24 @@ import { ServicesService } from '../service/services.service';
 
 export class HistoryComponent implements OnInit {
  ite:any = 0;
-  constructor(private dataService:ServicesService) { }
+  constructor(private dataService:ServicesService,public dialog: MatDialog) { }
   uid:any;
   historyData:any;
   itemData:any;
   ngOnInit(): void {
     this.uid = sessionStorage.getItem("id")
     this.dataService.viewOrder(this.uid).subscribe(data => {
-    this.historyData = data
+    this.itemData = data
      alert(data);
      console.log(data);
-    console.log(this.historyData);
+    console.log(this.itemData);
     
     })
   }
+  openDialog(product:any){
+    this.dialog.open(HistoryDetailsComponent,{data:product});
+  }
+
   cancelRequest(){
     alert("send request to admin")
   }
